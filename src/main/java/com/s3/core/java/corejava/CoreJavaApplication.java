365c9acd2dc2b550537443ebb85542c95df8bfde
Satya Thapa <satyapthapa@gmail.com>
2021-01-02 21:42:44 -0600

    create a console class

--- src/main/java/com/s3/core/java/corejava/CoreJavaApplication.java
+++ src/main/java/com/s3/core/java/corejava/CoreJavaApplication.java
@@ -1,13 +1,18 @@
 package com.s3.core.java.corejava;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoreJavaApplication {
+public class CoreJavaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoreJavaApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("My first springboot command line runner");
+	}
 }

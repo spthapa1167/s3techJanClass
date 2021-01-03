f1606b52639bfec797e347e6d1f2fdcf8cd56cd6
Satya Thapa <satyapthapa@gmail.com>
2021-01-02 21:08:22 -0600

    Initial Project Setup

---
+++ src/main/java/com/s3/core/java/corejava/CoreJavaApplication.java
@@ -0,0 +1,13 @@
+package com.s3.core.java.corejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreJavaApplication.class, args);
+	}
+
+}
